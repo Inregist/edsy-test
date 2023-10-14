6508034de694a0a70e7c92ff885939ad9aaa1510
inregist <inregist.irg@gmail.com>
2023-10-14 06:17:56 +0000

    add navbar

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,16 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { NavBar } from '@/components/Navbar';
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
+        <NavBar />
+        <div className="h-12" />
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
